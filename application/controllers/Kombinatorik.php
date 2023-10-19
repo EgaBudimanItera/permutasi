2dbf52328e97358c6a014bca489961ccc98fcc01
ega budiman <ega.budiman@staff.itera.ac.id>
2023-10-19 10:28:37 +0700

    up

--- application/controllers/Kombinatorik.php
+++ application/controllers/Kombinatorik.php
@@ -3,25 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kombinatorik extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function index()
 	{
 		$n = 5; // Starting number
-    $r = 5; // Ending number
+    	$r = 5; // Ending number
 		$n=$this->input->get('nilai_n');
 		$r=$this->input->get('nilai_r');
 		empty($n)||$n=="" || $n>7?$n=5:$n=$this->input->get('nilai_n');
